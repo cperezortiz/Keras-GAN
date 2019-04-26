439ee7c7d3d60fcc69624b9ba17cea1c22b9931b
cperezo <cperezo@ncsu.edu>
2019-04-26 00:28:26 -0400

    path

--- srgan/data_loader.py
+++ srgan/data_loader.py
@@ -11,7 +11,7 @@ class DataLoader():
     def load_data(self, batch_size=1, is_testing=False):
         data_type = "train" if not is_testing else "test"
         
-        path = glob('./datasets/%s/*' % (self.dataset_name))
+        path = glob('./datasets/%s/*' % (data_type))
 
         batch_images = np.random.choice(path, size=batch_size)
 
